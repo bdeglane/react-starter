9bd1f3eaaa86fe1ca0e3f0c6e41fca7703ac9307
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-05-16 22:57:27 +0200

    clean files

--- app/main.js
+++ app/main.js
@@ -1,28 +1,19 @@
 import '../public/bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import '../style/style.scss';
 
-// import 'file?name=[name].[ext]!./index.html';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router';
 
-import Container from './container/container.jsx';
 import MainLayout from './container/MainLayout.jsx';
-import User from './component/user/User.jsx';
-
-// let hello = 'Name';
-// let items = ['Roger', 'Simon', 'Françoise'];
-
-// ReactDOM.render(
-//     <Container hello={hello} items={items}/>, document.getElementById('app'));
+import Home from './container/page/Home.jsx';
+import Chat from './container/page/Chat.jsx';
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route component={MainLayout}>
-      <Route path="/" component={Container}>
-        <Route path="user/:name" component={User}/>
-      </Route>
+        <Route path="/" component={Home}/>
+        <Route path="/chat" component={Chat}/>
     </Route>
   </Router>
   , document.getElementById('app')
