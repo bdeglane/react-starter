39d512ccb8bae03691b3ca087aa2cc93db83c835
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-05-16 22:56:13 +0200

    create pages

---
+++ app/container/page/Chat.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Chat extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div ClassName='chat'>
+            </div>
+        )
+    }
+}
---
+++ app/container/page/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="jumbotron home">
+                <h1>React Bootstrap</h1>
+                <p>Simple bootstrap for react using react-router</p>
+            </div>
+        )
+    }
+}
