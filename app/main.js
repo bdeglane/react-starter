b6a4856b0020bea31c6fda156c03690863f711c6
compas-tis <benoit.deglane@compas-tis.com>
2016-05-17 11:03:34 +0200

    clean main

--- app/main.js
+++ app/main.js
@@ -1,20 +1,4 @@
-import '../public/bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss';
-import '../style/style.scss';
+import ReactBootstrap from './ReactBootstrap.jsx';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
-
-import MainLayout from './container/MainLayout.jsx';
-import Home from './container/page/Home.jsx';
-import Chat from './container/page/Chat.jsx';
-
-ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route component={MainLayout}>
-        <Route path="/" component={Home}/>
-        <Route path="/chat" component={Chat}/>
-    </Route>
-  </Router>
-  , document.getElementById('app')
-);
+let app = new ReactBootstrap();
+app.render();
