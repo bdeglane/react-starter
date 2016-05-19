16d6f308f9c09e8c8775e624bf0bfc48433f83ac
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-05-19 22:04:31 +0200

    add redux

--- app/main.js
+++ app/main.js
@@ -3,18 +3,24 @@ import '../style/style.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+
+import {Router, Route, browserHistory} from 'react-router';
+
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 
 import MainLayout from './container/MainLayout.jsx';
 import Home from './container/page/Home.jsx';
 import Chat from './container/page/Chat.jsx';
 
+let store = createStore(() => {});
+
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route component={MainLayout}>
-        <Route path="/" component={Home}/>
-        <Route path="/chat" component={Chat}/>
-    </Route>
-  </Router>
-  , document.getElementById('app')
-);
+  <Provider store={store}>
+    <Router history={browserHistory}>
+        <Route component={MainLayout}>
+            <Route path="/" component={Home}/>
+            <Route path="/chat" component={Chat}/>
+        </Route>
+    </Router>
+</Provider>, document.getElementById('app'));
